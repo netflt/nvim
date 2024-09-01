3a8c5798ea3c279d5ad082dfe3b9baf55ecaa1a3
Danny Zhu <danny@netflt.com>
2024-09-01 23:02:42 +0800

    add breakpoint types

--- lua/plugins/cppdbg.lua
+++ lua/plugins/cppdbg.lua
@@ -28,8 +28,7 @@ return {
     {
         "mfussenegger/nvim-dap",
         event = "VeryLazy",
-        keys = {
-                    },
+        keys = {},
         config = function()
             local dap = require("dap")
             dap.adapters.lldb = {
@@ -42,6 +41,16 @@ return {
             --dap.configurations.c = dap.configurations.cpp
             --dap.configurations.rust = dap.configurations.cpp
             --mapping = require("config.keymaps").mapDAP()
+            vim.api.nvim_set_hl(0, "blue",   { fg = "#3d59a1" }) 
+            vim.api.nvim_set_hl(0, "green",  { fg = "#9ece6a" }) 
+            vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" }) 
+            vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" }) 
+
+            vim.fn.sign_define('DapBreakpoint',          { text='', texthl='blue',   linehl='DapBreakpoint', numhl='DapBreakpoint' })
+            vim.fn.sign_define('DapBreakpointCondition', { text='', texthl='blue',   linehl='DapBreakpoint', numhl='DapBreakpoint' })
+            vim.fn.sign_define('DapBreakpointRejected',  { text='', texthl='orange', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+            vim.fn.sign_define('DapStopped',             { text='', texthl='green',  linehl='DapBreakpoint', numhl='DapBreakpoint' })
+            vim.fn.sign_define('DapLogPoint',            { text='󰮍', texthl='yellow', linehl='DapBreakpoint', numhl='DapBreakpoint' })
         end,
     },
 }
