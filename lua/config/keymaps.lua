5da4e0712cbd613647e34723f167516fd3db8495
Danny Zhu <danny@netflt.com>
2024-09-21 22:33:48 +0800

    add hotkey for open specify commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -65,14 +65,13 @@ map("n", "<leader>ci", ":CMakeInstall<CR>", opt)
 map("n", "<leader>gv", ":DiffviewOpen<CR>", opt)
 map("n", "<leader>go", ":DiffviewClose<CR>", opt)
 
-function OpenCurrentChanges()
+function GetCommitSHA()
     local api = vim.api
     local cache = require('gitsigns.cache').cache
     local bufnr = api.nvim_get_current_buf()
     local bcache = cache[bufnr]
     if not bcache then
-        api.nvim_command('DiffviewFileHistory %')
-        return
+        return ""
     end
 
     bcache:get_blame()
@@ -81,14 +80,33 @@ function OpenCurrentChanges()
     local cursor = unpack(api.nvim_win_get_cursor(blm_win))
     local cur_sha = blame[cursor].commit.abbrev_sha
     if string.match(cur_sha, "00000000") then
+        return ""
+    end
+    return cur_sha
+end
+
+function OpenCurrentFileChange()
+    local api = vim.api
+    local cur_sha = GetCommitSHA()
+    if cur_sha == "" then
         api.nvim_command('DiffviewFileHistory %')
     else
         api.nvim_command('DiffviewFileHistory % --range='.. cur_sha)
     end
 end
 
-map("n", "<leader>gf", ":lua OpenCurrentChanges()<CR>", opt)
+function OpenCurrentCommit()
+    local api = vim.api
+    local cur_sha = GetCommitSHA()
+    if cur_sha == "" then
+        api.nvim_command('DiffviewOpen')
+    else
+        api.nvim_command('DiffviewOpen ' .. cur_sha .. '^!')
+    end
+end
 
+map("n", "<leader>gf", ":lua OpenCurrentFileChange()<CR>", opt)
+map("n", "<leader>gc", ":lua OpenCurrentCommit()<CR>", opt)
 --dap debug
 --map ("n",
 --    "<leader>ds",
