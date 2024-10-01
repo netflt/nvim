57c1a2e7d9db8a9592079fb13f9ad7093e3b9cc4
Danny Zhu <danny@netflt.com>
2024-10-01 19:20:30 +0800

    use vgit plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -56,18 +56,4 @@ return {
         "RRethy/vim-illuminate",
         event = "VeryLazy"
     },
-    {
-        "rmagatti/goto-preview",
-        event = BufReadPre,
-        config = function()
-            require("goto-preview").setup({
-                width = 120,
-                height = 25,
-                default_mappings = true,
-                debug = false,
-                opacity = nil,
-                post_open_hook = nil, 
-            })
-        end,
-    },
 }
