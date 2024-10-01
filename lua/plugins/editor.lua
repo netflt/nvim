19eef8acf16e4a386e8c264865d63518b68f857a
Danny Zhu <danny@netflt.com>
2024-10-01 15:14:40 +0800

    update config

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,73 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+    -- disable plugins
+    { "folke/lazydev.nvim", enabled = false },
+    { "folke/tokyonight.nvim", enabled = false },
+    { "catppuccin/nvim", enabled = false },
+    { "folke/flash.nvim", enabled = false },
+    { "echasnovski/mini.ai", enabled = false },
+    { "williamboman/mason.nvim", enabled = false },
+    { "williamboman/mason-lspconfig.nvim", enabled = false },
+    { "stevearc/dressing.nvim", enabled = false },
+    { "Bilal2453/luvit-meta", enabled = false },
+    { "mfussenegger/nvim-lint", enabled = false },
+    { "nvimdev/dashboard-nvim", enabled = false },
+    { "windwp/nvim-ts-autotag", enabled = false },
+    { "garymjr/nvim-snippets", enabled = false },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = "VeryLazy",
+        opts = {
+            ensure_installed = { "cpp", "c", "json", "bash", "css", "vim", "lua", "javascript", "typescript", "tsx" },
+            -- 启用代码高亮模块
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            -- 启用增量选择模块
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<CR>",
+                    node_incremental = "<CR>",
+                    node_decremental = "<BS>",
+                    scope_incremental = "<TAB>",
+                },
+            },
+            -- 启用代码缩进模块 (=)
+            indent = {
+                enable = true,
+            },
+        },
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            { "<leader><space>" , ":", desc = "Switch Command mode",},
+        },
+    },
+    {
+        "RRethy/vim-illuminate",
+        event = "VeryLazy"
+    },
+    {
+        "rmagatti/goto-preview",
+        event = BufReadPre,
+        config = function()
+            require("goto-preview").setup({
+                width = 120,
+                height = 25,
+                default_mappings = true,
+                debug = false,
+                opacity = nil,
+                post_open_hook = nil, 
+            })
+        end,
+    },
+}
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,34 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    default_component_configs = {
+        indent = {
+            highlight = "NeoTreeIndentMarker",
+            -- expander config, needed for nesting files
+            with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "",
+            default = "*",
+            highlight = "NeoTreeFileIcon"
+        },
+        
+        git_status = {
+            symbols = {
+                -- Status type
+                untracked = "",
+                ignored   = "",
+                unstaged  = "󰄱",
+                staged    = "",
+                conflict  = "",
+            }
+        },
+    },
     filesystem = {
         filtered_items = {
             visible = true,
